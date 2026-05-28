fe5e46aae568fa0aa9f2204343d1f8d8ceb43254
panando <liupengsyd@qq.com>
2026-05-28 11:09:40 +0800

    feat: add environmentVariables to ProviderConfig

--- APIBypass/Models/ProviderConfig.swift
+++ APIBypass/Models/ProviderConfig.swift
@@ -5,16 +5,46 @@ struct ProviderConfig: Codable, Identifiable, Equatable {
     var name: String
     var apiProvider: APIProvider
     var baseURL: URL
+    var environmentVariables: [EnvironmentVariableConfig]
 
     init(
         id: UUID = UUID(),
         name: String,
         apiProvider: APIProvider,
-        baseURL: URL
+        baseURL: URL,
+        environmentVariables: [EnvironmentVariableConfig]? = nil
     ) {
         self.id = id
         self.name = name
         self.apiProvider = apiProvider
         self.baseURL = baseURL
+        self.environmentVariables = environmentVariables ?? ProviderConfig.defaultEnvironmentVariables()
+    }
+
+    /// 预设的环境变量模板
+    static func defaultEnvironmentVariables() -> [EnvironmentVariableConfig] {
+        [
+            EnvironmentVariableConfig(
+                id: UUID(),
+                name: "ANTHROPIC_BASE_URL",
+                value: "",
+                type: .baseURL,
+                isEnabled: true
+            ),
+            EnvironmentVariableConfig(
+                id: UUID(),
+                name: "ANTHROPIC_MODEL",
+                value: "",
+                type: .modelMapping,
+                isEnabled: true
+            ),
+            EnvironmentVariableConfig(
+                id: UUID(),
+                name: "ANTHROPIC_AUTH_TOKEN",
+                value: "",
+                type: .keychainToken,
+                isEnabled: true
+            )
+        ]
     }
 }
